99fcab3753bb8a699ebc7182d59012f6cc50e739
2daysweb <sahnunhm@gmail.com>
2019-07-14 09:16:17 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js';
-import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
-import MouseComponent from './MouseComponent';
+import EinsteinQuoteComponent from './EinsteinQuoteComponent.js';
+import MouseComponent from './MouseComponent.js';
+
+// import React, { Component } from 'react';
+// import moment from 'moment';
+// import ExampleComponent from './ExampleComponent'
+// import TestComponent from './TestComponent'
+
 
 class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
 			<div className="App">
+			<CatComponent/>
+			<EinsteinQuoteComponent/>
+			<MouseComponent/>
 			</div>
 		);
 	}
 }
 
 export default App;
+
+
+// ## Deliverables
+
+// - Two components have not been properly imported in `src/App.js`. Identify and debug
+//   the issue (_hint_: the stack trace when running `npm test` should point you in
+//   the right direction!)
+
+// - `App` needs to render three children components:
+
+//   1.  `CatComponent`
+//   2.  `EinsteinQuoteComponent`
+//   3.  `MouseComponent`
